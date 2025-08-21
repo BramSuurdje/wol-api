b74a74e696992ae67b3d16c1953bcb5f7ec295cc
Bram Suurd <78373894+BramSuurdje@users.noreply.github.com>
2025-08-21 03:31:53 +0200

    chore: add ESLint configuration and update dependencies for improved code quality and consistency

---
+++ eslint.config.mjs
@@ -0,0 +1,42 @@
+// Run this command to generate base config and vs code settings:
+// bunx @antfu/eslint-config@latest
+
+import antfu from "@antfu/eslint-config";
+
+export default antfu(
+  {
+    type: "app",
+    typescript: true,
+    formatters: true,
+    stylistic: {
+      indent: 2,
+      semi: true,
+      quotes: "double",
+    },
+  },
+  {
+    rules: {
+      "ts/no-redeclare": "off",
+      "ts/consistent-type-definitions": ["error", "type"],
+      "no-console": ["warn"],
+      "antfu/no-top-level-await": ["off"],
+      "node/prefer-global/process": ["off"],
+      "node/no-process-env": ["error"],
+      "perfectionist/sort-imports": [
+        "error",
+        {
+          type: "line-length",
+          order: "desc",
+        },
+      ],
+
+      "unicorn/filename-case": [
+        "error",
+        {
+          case: "kebabCase",
+          ignore: ["README.md"],
+        },
+      ],
+    },
+  }
+);
